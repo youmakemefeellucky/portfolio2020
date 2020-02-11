a1e4741a27addf35105108ce083047f64f1bc0a7
youmakemefeellucky <38502195+youmakemefeellucky@users.noreply.github.com>
2020-02-11 20:13:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+var x = document.getElementById("CV");
+var y = document.getElementById("cvButton");
+var z = document.getElementById("exitButton");
+var xx = document.getElementById("wrapper");
+
 function openCV() {
-  var x = document.getElementById("CV");
   if (x.style.display === "none") {
     x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "block";
+    xx.style.display = "none";
   } else {
     x.style.display = "none";
+    y.style.display = "block";
+    xx.style.display = "block";
+  }
+}
+
+function closeCV() {
+  if (x.style.display === "block" && z.style.display === "block") {
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
+} else {
+  x.style.display = "block";
+  z.style.display = "block";
   }
 }
